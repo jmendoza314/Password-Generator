299ce02c035131482421b083bd142fd447d4d26e
jeremym <jeremymendoza05@yahoo.com>
2021-05-29 22:03:30 -0700

    added function to generate password

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // Function that generates the random characters
+  function generatePassword() {
+    var length = 8
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVQXYZ1234567890!@#$%^&*"
+    value = ""
+    for (var i = 0; i <= chars.length; ++i) {
+    value = chars.charAt(Math.floor(Math.random() * chars.length +1));
+
+    return value;
+      }   
+}
   passwordText.value = password;
 
 }
+generateBtn.addEventListener("click", writePassword);   
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
